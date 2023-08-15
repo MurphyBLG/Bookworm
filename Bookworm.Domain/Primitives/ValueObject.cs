26085361abf04b5d5e0fc7fed526c7ea2ccf6560
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-15 21:27:53 +0500

    Value object abstract class created.

---
+++ Bookworm.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,24 @@
+﻿namespace Bookworm.Domain.Primitives;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj) =>
+        obj is ValueObject valueObject && GetEqualityComponents()
+            .SequenceEqual(valueObject.GetEqualityComponents());
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+        => Equals(left, right);
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+        => !Equals(left, right);
+
+    public override int GetHashCode()
+        => GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x ^ y);
+
+    public bool Equals(ValueObject? other)
+        => Equals((object?)other);
+}
