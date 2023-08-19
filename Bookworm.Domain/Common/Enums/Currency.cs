3229d91e83c727c7f262a96b4060ef9da936724e
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-19 17:32:03 +0500

    Created currency enum

---
+++ Bookworm.Domain/Common/Enums/Currency.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bookworm.Domain.Common.Enums;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public enum Currency    
+{
+    USD = 0,
+    EUR = 1,
+    RUB = 2
+}
