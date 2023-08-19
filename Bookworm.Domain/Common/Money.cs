6bf11ab2ca0ad8fb7633b287208e7dbb1f1e6204
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-19 17:32:18 +0500

    Money value object created

---
+++ Bookworm.Domain/Common/Money.cs
@@ -0,0 +1,25 @@
+﻿using Bookworm.Domain.Common.Enums;
+using Bookworm.Domain.Primitives;
+
+namespace Bookworm.Domain.Common;
+
+public class Money : ValueObject
+{
+    public decimal Value { get; set; }
+    public Currency Currency { get; set; }
+
+    private Money(decimal value, Currency currencyCode)
+    {
+        Value = value;
+        Currency = currencyCode;
+    }
+
+    public static Money Create(decimal value, Currency currencyCode)
+        => new(value, currencyCode);
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+        yield return Currency;
+    }
+}
