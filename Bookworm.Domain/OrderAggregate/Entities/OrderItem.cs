2b18435c68ad60cdfe172d9c53cae6f06b696a7d
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-20 19:45:57 +0500

    Namespace move and reconfigure of classes

---
+++ Bookworm.Domain/OrderAggregate/Entities/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using Bookworm.Domain.OrderAggregate.Ids;
+using Bookworm.Domain.Primitives;
+using Bookworm.Domain.ProductAggregate.Ids;
+
+namespace Bookworm.Domain.OrderAggregate.Entities;
+
+public sealed class OrderItem : Entity<OrderItemId>
+{
+    public OrderId OrderId { get; private set; }
+    public ProductId ProductId { get; private set; }
+    public int Amount { get; private set; }
+
+    private OrderItem(
+        OrderItemId id,
+        OrderId orderId,
+        ProductId productId) 
+        : base(id)
+    {
+        OrderId = orderId;
+        ProductId = productId;
+        Amount = 1;
+    }
+
+    public static OrderItem Create(OrderId orderId, ProductId productId)
+        => new(new OrderItemId(Guid.NewGuid()), orderId, productId);
+
+    public void AddItem()
+    {
+        Amount++;
+    }
+}
