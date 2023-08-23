c0feb3d287ae773cc5e7b14a6e8b24026f963cda
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-23 20:12:47 +0500

    Product create fabric method created

--- Bookworm.Domain/ProductAggregate/Product.cs
+++ Bookworm.Domain/ProductAggregate/Product.cs
@@ -6,11 +6,17 @@ namespace Bookworm.Domain.ProductAggregate;
 
 public class Product : Entity<ProductId>
 {
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public string Name { get; private set; } = null!;
+    public string Description { get; private set; } = null!;
     public Money Price { get; private set; } = null!;
 
-    public Product(ProductId id) : base(id)
+    private Product(ProductId id, string name, string description, Money price) : base(id)
     {
+        Name = name;
+        Description = description;
+        Price = price;
     }
+
+    public static Product Create(string name, string description, Money price)
+        => new(new ProductId(Guid.NewGuid()), name, description, price);
 }
