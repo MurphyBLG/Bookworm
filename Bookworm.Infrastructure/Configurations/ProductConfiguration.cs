6ea5377ac0f1e38ebfa22fa765e88d87c740b4d8
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-23 21:43:14 +0500

    Product configuration added

---
+++ Bookworm.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Bookworm.Domain.ProductAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookworm.Infrastructure.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+
+    }
+}
