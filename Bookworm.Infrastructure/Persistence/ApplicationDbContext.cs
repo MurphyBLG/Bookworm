89c160fde3453515e376c3de2aab3a2a0fb6e50f
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-23 20:47:04 +0500

    DbContext configured

---
+++ Bookworm.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookworm.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.UseNpgsql();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
