e693b37d6650fc03b04f93ef9ba82811b39943cc
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-24 20:27:44 +0500

    Product configuration created

--- Bookworm.Infrastructure/Configurations/ProductConfiguration.cs
+++ Bookworm.Infrastructure/Configurations/ProductConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Bookworm.Domain.ProductAggregate;
+using Bookworm.Domain.ProductAggregate.Ids;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,6 +9,22 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
+        builder.HasKey(p => p.Id);
 
+        builder.Property(p => p.Id)
+            .HasConversion(productId => productId.Value,
+                value => new ProductId(value));
+
+        builder.Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(255);
+
+        builder.OwnsOne(p => p.Price, priceBuilder =>
+        {
+            priceBuilder.Property(m => m.Currency).HasMaxLength(3);
+        });
     }
 }
