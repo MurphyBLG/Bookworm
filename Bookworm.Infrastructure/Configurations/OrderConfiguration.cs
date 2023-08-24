820c0bf35a001c0033839d860ea86af291bb6bb5
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-08-24 20:58:43 +0500

    Order configuration done

---
+++ Bookworm.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Bookworm.Domain.CustomerAggregate;
+using Bookworm.Domain.OrderAggregate;
+using Bookworm.Domain.OrderAggregate.Ids;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookworm.Infrastructure.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .HasConversion(orderId => orderId.Value,
+                value => new OrderId(value));
+
+        builder.HasOne<Customer>()
+            .WithMany()
+            .HasForeignKey(o => o.CustomerId)
+            .IsRequired();
+
+        builder.HasMany(o => o.OrderItems)
+            .WithOne()
+            .HasForeignKey(oi => oi.OrderId);
+    }
+}
